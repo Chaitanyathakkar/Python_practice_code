95a6f6ce23bb2011798a0ff56cc422f9474fa5cc
Chaitanyathakkar <157213443+Chaitanyathakkar@users.noreply.github.com>
2026-02-16 15:42:57 +0530

    Add files via upload

---
+++ Python Learning/Practice_prog5.py
@@ -0,0 +1,75 @@
+# dic = {
+#     "Abhi" : "now",
+#     "kal" : "tomorrow",
+#     "sathi" : "Partner"
+# }
+
+# word = input("Enter the word you want meaning of ")
+# print(dic[word])
+
+
+
+# ================ set unique nos ==================
+
+
+# s = set()
+
+
+# s.add(int(input("Enter a number: ")))
+# s.add(int(input("Enter a number: ")))
+# s.add(int(input("Enter a number: ")))
+# s.add(int(input("Enter a number: ")))
+# s.add(int(input("Enter a number: ")))
+# s.add(int(input("Enter a number: ")))
+# s.add(int(input("Enter a number: ")))
+# s.add(int(input("Enter a number: ")))
+
+# print(s)
+
+
+# =============== set ================
+
+
+# s = set()
+
+# s.add(20)
+# s.add(20.0)
+# s.add('20')
+
+# print(len(s))
+
+
+
+
+# ================= dict ==================
+
+
+# dic = {}
+
+
+# name = input("Enter your name: ")
+# lang = input("Enter your favourite language: ")
+# dic.update({name:lang})
+
+# name = input("Enter your name: ")
+# lang = input("Enter your favourite language: ")
+# dic.update({name:lang})
+
+# name = input("Enter your name: ")
+# lang = input("Enter your favourite language: ")
+# dic.update({name:lang})
+
+# name = input("Enter your name: ")
+# lang = input("Enter your favourite language: ")
+# dic.update({name:lang})
+
+# print(dic)
+
+
+
+# ================ list in set ==================
+
+
+s = {8, 7, 12, 'Harry', [1,2]}
+
+print(s)
